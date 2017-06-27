9043dcbfd39b9ae3d1ab911686f5a57120bcd8c0
Nan Xiao <nanxiao@uw.edu>
2017-06-27 14:26:34 -0700

    Add files via upload

---
+++ mc-example.php
@@ -0,0 +1,30 @@
+<?php
+//fill in these values for with your own information
+$api_key = 'a243cde1dfbb52925c0d6d7c4aa94900-us7';
+$datacenter = 'us7';
+$list_id = '03336bdae1';
+$email = $_POST['email'];
+$status = 'pending';
+if(!empty($_POST['status'])){
+    $status = $_POST['status'];
+}
+$url = 'https://'.$datacenter.'.api.mailchimp.com/3.0/lists/'.$list_id.'/members/';
+$username = 'apikey';
+$password = $api_key;
+$data = array("email_address" => $email,"status" => $status);
+$data_string = json_encode($data);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, "$username:$api_key");
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string))
+);
+$result=curl_exec ($ch);
+curl_close ($ch);
+echo $result;
+?>
